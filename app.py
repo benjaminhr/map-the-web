1b0863fe4e29508a99e90a9d6ea35f1246dc1a76
benjaminr177 <unoceptik@gmail.com>
2019-05-10 00:07:59 +0100

    add basic auth

--- app.py
+++ app.py
@@ -2,13 +2,28 @@ import os
 from flask import Flask, request
 from utils.search import Search
 from flask_cors import CORS, cross_origin
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+users = {
+  "benjamin": "robson",
+  "karmjit": "10252"
+}
+
+@auth.get_password
+def get_pw(username):
+  if username in users:
+    return users.get(username)
+  
+  return None
+
 @app.route('/search', methods=['POST'])
 @cross_origin()
+@auth.login_required
 def search():
   req_data = request.get_json()
 
